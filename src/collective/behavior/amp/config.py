d2b3afd096cbfc8c79f0dce1e501f9a109970fb5
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2016-10-03 10:48:43 -0300

    Transform RichText fields to remove disallowed tags (#11)
    * Transform RichText fields to remove disallowed tags
    
    * Refactor
    
    * Invert logic to filter invalid tags

--- src/collective/behavior/amp/config.py
+++ src/collective/behavior/amp/config.py
@@ -1,2 +1,23 @@
 # -*- coding: utf-8 -*-
 PROJECTNAME = 'collective.behavior.amp'
+
+# https://github.com/ampproject/amphtml/blob/master/spec/amp-html-format.md
+AMP_INVALID_ELEMENTS = [
+    'base',
+    'img',
+    'video',
+    'audio',
+    'iframe',
+    'frame',
+    'frameset',
+    'object',
+    'param',
+    'applet',
+    'embed',
+    'form',
+    'input',
+    'textarea',
+    'select',
+    'option',
+    'style',  # valid just one tag style in the head
+]
