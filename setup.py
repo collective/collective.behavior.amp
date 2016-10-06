7f0c67e83728d360e72961760dcef044c445dec7
hvelarde <hector.velarde@gmail.com>
2016-10-06 17:32:28 -0300

    Back to development: 1.0a2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0a1'
+version = '1.0a2.dev0'
 description = 'Support for Accelerated Mobile Pages (AMP) on Dexterity-based content types.'
 long_description = (
     open('README.rst').read() + '\n' +
